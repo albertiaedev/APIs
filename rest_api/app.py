7eab6532efafb759adf6c20e31d7949dad2bcf29
J.A. Hernández <albertiae.digital@gmail.com>
2023-02-04 20:28:30 -0400

    Delete an item

--- rest_api/app.py
+++ rest_api/app.py
@@ -13,6 +13,14 @@ class Item(BaseModel):
 def read_root():
   return {"REST API CRUD built on FastAPI":"by J.A. Hernández"}
 
+'''
+
+The next code is for a REST API with basic CRUD operations.
+
+Run this app using the command `uvicorn main:app --reload` and test it
+using Postman or Curl as API client.
+
+'''
 # Create an item
 @app.post("/item/")
 async def create_item(item: Item):
@@ -27,3 +35,8 @@ async def read_item(id: int, q: str=None):
 @app.put("/item/{id}")
 async def update_item(id: int, item: Item):
   return {"id": id, "item": item}
+
+# Delete an item
+@app.delete("/item/{id}")
+asyn def delete_item(id: int):
+  return {"id": id}
