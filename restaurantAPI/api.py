6abaadfff8d37abf8549cab972a2bc04b9106288
J.A. Hernández <albertiae.digital@gmail.com>
2023-02-07 10:57:02 -0400

    Created a class instance of the dishes in the menu

--- restaurantAPI/api.py
+++ restaurantAPI/api.py
@@ -1,2 +1,15 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+
+# Set the app
+
+app = FastAPI()
+
+
+# Create a class instance of the dishes in the menu
+
+class Dish(BaseModel):
+  name: str
+  description: str
+  price: float
+  tax: float
