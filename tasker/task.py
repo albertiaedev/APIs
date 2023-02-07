3fafdd21198ede51682a9c2d1b1beaf07fc52eb3
J.A. Hernández <albertiae.digital@gmail.com>
2023-02-06 20:53:20 -0400

    Created an instance class for the tasks and a list to store them in memory

--- tasker/task.py
+++ tasker/task.py
@@ -1,2 +1,14 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+
+app = FastAPI()
+
+# Define the model we´ll use for creating, reading, updating and deleting the tasks
+class Tasks(BaseModel):
+  task: str
+  description: str
+
+
+# Now we create a list to store tasks in memory
+# Use a SQL/NoSQL database to store the data when you deploy the app to production
+tasks = []
